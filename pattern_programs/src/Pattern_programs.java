a9ddbe952f28c8a405dc7bf0d781f246c2aa2be4
ritheshjarvis <ritheshjarvis@gmail.com>
2021-02-05 17:17:23 +0530

    Comments are updated.

--- pattern_programs/src/Pattern_programs.java
+++ pattern_programs/src/Pattern_programs.java
@@ -2,12 +2,19 @@ public class Pattern_programs {
     public static void main(String[] args) {
         int num = 5;
         for(int i = 0; i < num; i++){
+
+            //First inner loop for Blank spaces - First line
+            //In first iteration Four space will be printed
             for(int j = i; j < num-1; j++) {
                 System.out.print(" ");
             }
+
+            //Second inner loop for star - Second line
+            //In first iteration one star will be printed
             for(int k = 0; k <= i; k++) {
                 System.out.print("*");
             }
+
             System.out.println();
         }
     }
